9aae4db4c1d246279a8039460f7bc8b3ac1eaf3f
FrankOcrospomaUgaz <anthony.2005.rap@gmail.com>
2026-06-04 12:45:48 -0500

    feat: add vite.config.js for project configuration; integrate Tailwind CSS and set up development server with Laravel and React plugins

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+export default defineConfig({
+    plugins: [
+      laravel({
+        input: ["resources/css/app.css", "resources/js/main.tsx"],
+        refresh: true,
+      }),
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "resources/js"),
+      },
+    },
+    server: {
+      watch: {
+        ignored: ["**/storage/framework/views/**"],
+      },
+    },
+  }); 
